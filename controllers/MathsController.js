b63505de9d3deb9606411ac809906311600b1394
Samuel-Gourgue <s.gourgue@live.com>
2024-10-03 18:15:56 -0400

    Edit 12

--- controllers/MathsController.js
+++ controllers/MathsController.js
@@ -2,10 +2,8 @@ import Controller from './Controller.js';
 
 export default class MathsController extends Controller {
     constructor(HttpContext) {
-        super(HttpContext);
         this.HttpContext = HttpContext;
     }
-
     async get() {
         const { op, x, y, n } = this.HttpContext.query;
 
