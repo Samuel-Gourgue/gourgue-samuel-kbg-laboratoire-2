e65b58aeecea4908d6dd8fbfa2ad7514ac7b0412
Samuel-Gourgue <s.gourgue@live.com>
2024-10-03 17:30:11 -0400

    Export dans cors.js

--- cors.js
+++ cors.js
@@ -1,13 +1,12 @@
 function allowAllAnonymousAccess(res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
     res.setHeader('Access-Control-Expose-Headers', '*');
 }
 
 function accessControlConfig(httpContext) {
-    console.log('CORS request from origin:', httpContext.req.headers['origin']);
-    if (httpContext.req.headers['sec-fetch-mode'] === 'cors') {
+    if (httpContext.req.headers['sec-fetch-mode'] == 'cors') {
         allowAllAnonymousAccess(httpContext.res);
     }
 }
@@ -15,10 +14,11 @@ function accessControlConfig(httpContext) {
 export function handleCORSPreflight(httpContext) {
     accessControlConfig(httpContext);
     if (httpContext.req.method === 'OPTIONS') {
-        console.log('CORS preflight verification for method:', httpContext.req.method);
-        httpContext.res.writeHead(204);
+        console.log('CORS preflight verifications');
         httpContext.res.end();
         return true;
     }
     return false;
 }
+
+export { allowAllAnonymousAccess };
